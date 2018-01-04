b1485db27028504abea895d4d97f735a883888d3
Christopher Grebs <cg@webshox.org>
2018-01-04 10:42:42 +0100

    Add django 2 compatibility, test on django 2.
    This removes support for Django 1.9 and 1.10.
    
    Fixes #14

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import codecs
 from setuptools import setup, find_packages
 
 
-version = '1.1'
+version = '1.2'
 
 
 if sys.argv[-1] == 'publish':
@@ -23,7 +23,7 @@ def read(*parts):
 
 
 install_requires = [
-    'Django>=1.8,<1.11',
+    'Django>=1.8,<2.1',
     'm2secret-py3>=1.3'
 ]
 
